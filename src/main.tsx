22d860a0f0aea326aaf2fa8a4c2f571ac998a782
andreamichelifrog <andrea.a.micheli@capgemini.com>
2025-10-27 16:56:37 +0100

    further testing routing config

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,17 @@
 import ReactDOM from "react-dom/client";
-import {
-  RouterProvider,
-  createMemoryHistory,
-  createRouter,
-} from "@tanstack/react-router";
+import { createHashHistory } from "@tanstack/history";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
 
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
 import { UserProvider } from "./lib/UserContext";
 import { ThemeProvider } from "./lib/ThemeContext";
 
-const memoryHistory = createMemoryHistory({
-  initialEntries: ["/"],
-});
-
 // Create a new router instance
 const router = createRouter({
   routeTree,
   basepath: "/me",
-  history: memoryHistory,
+  history: createHashHistory(),
 });
 
 // Register the router instance for type safety
